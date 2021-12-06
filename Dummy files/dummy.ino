a645ac715c1ef50e8e86409e3e4b0be110905f9e
mrinal-kasyap <91459392+mrinalkasyap-qmetry@users.noreply.github.com>
2021-12-06 14:10:49 +0530

    Create dummy.ino

---
+++ Dummy files/dummy.ino
@@ -0,0 +1,15 @@
+int myNum; // declares an integer named myNum
+myNum= 13; // sets the integer named myNum equal to 13
+int myNum= 13; // simultaneously declares an integer named myNum and sets it equal to 13
+
+float myDec; // declares a decimal named myDec
+myDec= 13.001; // sets the decimal named myDecequal to 13.001
+float myDec= 13; // simultaneously declares a decimal named myDecand sets it equal to 13
+
+char theChar; // declares a character named theChar
+theChar = ‘a’; // sets the character named theChar equal to the letter ‘a’. Notice that the character is surrounded my apostrophes.
+char theChar = ‘c’; // simultaneously declares a character named myNum and sets it equal to the letter ‘a’. Notice that the character is surrounded by apostrophes.
+
+String someWord; // declares a String named someWord
+someWord = “Hello Scott”; // sets the String named someWord equal to “Hello Scott”. Notice that the text itself is surrounded by quotations.
+String someWord = “Goodbye Scott!”; // simultaneously declares a String named someWord and sets it equal to “Goodbye Scott!”. Notice that the text itself is surrounded by quotations.
